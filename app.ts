3edf0ff8c7e701ac769d719f71cf62a5ec3a86a5
Kamil Jakóbczak <k_j1@vp.pl>
2025-03-17 11:10:41 +0100

    add webpack configuration

---
+++ app.ts
@@ -0,0 +1,9 @@
+import './src/scss/main.scss';
+
+const wrapper = document.querySelector(
+	'.wrapper'
+) as HTMLElement;
+
+const testH1 = document.createElement('h1');
+testH1.textContent = 'Hello World!';
+wrapper.appendChild(testH1);
